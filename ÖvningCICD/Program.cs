0208d4dcd52ec3f56668560db4ec2ae1b384843d
preetidogra <70689452+preetidogra@users.noreply.github.com>
2021-10-27 10:25:54 +0200

    Add project files.

---
+++ ÖvningCICD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ÖvningCICD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
